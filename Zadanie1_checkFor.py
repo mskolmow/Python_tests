6f41913597fa00cbe0190deb3bea9579ab9a6072
mskolmow <mgrzt.skolmowska@gmail.com>
2018-07-20 11:01:45 +0200

    v.0.1 - Poprawione komentarze, czytelnosci

--- Zadanie1_checkFor.py
+++ Zadanie1_checkFor.py
@@ -1,6 +1,7 @@
 for i in range (0,3):
     print (i)
 #drukuje 0 1 2
+print('---------------------------------------------------------------')
 
 for i in range (0,3):
     j=i+1
@@ -8,30 +9,36 @@ for i in range (0,3):
     print ('j= ' + str(j))
 #dla i drukuje 0 1 2
 #dla j drukuje 1 2 3
+print('---------------------------------------------------------------')
 
 for i in range (0,3):
     i=i+1
     print ('i= ' + str(i))
 #dla i wydrukuje 1 2 3
+print('---------------------------------------------------------------')
 
 
 print('Parzyste')
 for i in range (1,100):
     if i%2==0:
         print(i)
+# liczby parzyste z przedziału od  1 do 99 łącznie 
+print('---------------------------------------------------------------')
 
 
 print('Nieparzyste')
 for i in range (1,100):
     if i%2!=0:
         print(i)
+# liczby nieparzyste z przedziału od  1 do 99 łącznie 
+print('---------------------------------------------------------------')
 
-
-#Ile jest liczb nieparzytych w zbiorze liczb 0-100
 List=[]
-for i in range (0,100):
+for i in range (0,101):
     if i%2!=0:
         List.append(i)
+        print(i)
 print('w przedziale 0-100 jest ' + str(len(List)) + ' liczb nieparzystych')
-
+#Ile jest liczb nieparzytych w zbiorze liczb 0-99
+print('---------------------------------------------------------------')
 
