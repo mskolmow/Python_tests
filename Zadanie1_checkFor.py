deaa13feab5b6b61d3fa18105a8796217b1c4c08
mskolmow <mgrzt.skolmowska@gmail.com>
2018-07-20 11:26:04 +0200

    poprawki czytelnosci kodu - wyswietlenie listy parzystych i nieparzystych liczb z przedzialu 0-100

--- Zadanie1_checkFor.py
+++ Zadanie1_checkFor.py
@@ -30,15 +30,22 @@ print('Nieparzyste')
 for i in range (1,100):
     if i%2!=0:
         print(i)
-# liczby nieparzyste z przedziału od  1 do 99 łącznie 
+    # liczby nieparzyste z przedziału od  1 do 99 łącznie 
 print('---------------------------------------------------------------')
 
 List=[]
 for i in range (0,101):
     if i%2!=0:
         List.append(i)
-        print(i)
-print('w przedziale 0-100 jest ' + str(len(List)) + ' liczb nieparzystych')
+print(List)
+print('w przedziale 0-100 jest: ' + str(len(List)) + ' liczb nieparzystych')
 #Ile jest liczb nieparzytych w zbiorze liczb 0-99
 print('---------------------------------------------------------------')
 
+List_niep=[]
+for i in range(0,101):
+    if  i%2==0:
+        List_niep.append(i)
+print(List_niep)
+print('W przedziale 0-100 jest: '+ str(len(List_niep)) + ' liczb parzystych')
+#Ile jest liczb parzytych w zbiorze liczb 0-100 łącznie + wyswietlenie liczb
