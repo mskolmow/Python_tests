9f7299929e73e9e4d02f7d1a4a401ddf47828bff
mskolmow <mgrzt.skolmowska@gmail.com>
2018-07-20 13:38:28 +0200

    dodanie przykladu do Zadanie2, gdzie pobierany jest parametr z klawaitury i uzyta pętla 'while'

--- Zadanie2_checkWhile.py
+++ Zadanie2_checkWhile.py
@@ -12,4 +12,12 @@ while i<0:
 #drukuje liczby od -10 do 0
 print('---------------------------------------------------------------')
 
+#ograniczenie dla uzytkownika aby podal LICZBE wieksza od zera
+print('Uzytkowniku - Podaj liczbe:')
+wej=input() #tutaj pobierany jest parametr od uzytkownika
 
+while float(wej)<=0:
+    print('Podaj liczbe wieksza od zera')
+    wej=input()
+print('Good!:)')
+print('---------------------------------------------------------------')
