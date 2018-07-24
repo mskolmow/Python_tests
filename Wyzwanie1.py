4cee7140432d9cf5b8086740101cda904ca93e88
mskolmow <mgrzt.skolmowska@gmail.com>
2018-07-24 23:06:32 +0200

    Update Wyzwanie1 i dodanie Wyzwanie 2

--- Wyzwanie1.py
+++ Wyzwanie1.py
@@ -8,4 +8,16 @@ sen_list=list(sen)
 tablica=[]
 for i in range (0,len(sen_list)):
     tablica.append(sen_list[i])
-print(tablica [::-1])
+#Poniżej dzieje się WAŻNA rzecz!
+#odwracamy zawartość indeksów - co jest oczywiste :)
+#ale wciąż to tablica,aby zamienić ją na string używamy .join
+
+#zalozmy,ze uzytkownik podaje zdanie : 'Gosia ma Tifcie'
+print("".join(tablica [::-1])) # tuta 'eicfiT am aisoG'
+
+
+print(tablica [::-1]) #tutaj '['e', 'i', 'c', 'f', 'i', 'T', ' ', 'a', 'm', ' ', 'a', 'i', 's', 'o', 'G']'
+#poniżej przetestujmy wartości co sie wstanie jak powstawiamy rozne znaki w (...).join
+
+print(" ".join(tablica [::-1])) #tutaj 'e i c f i T   a m   a i s o G'
+print("X".join(tablica [::-1])) #tutaj 'eXiXcXfXiXTX XaXmX XaXiXsXoXG'
---
+++ Wyzwanie2.py
@@ -0,0 +1,11 @@
+#Wyzwanie2 to podzielenie ciagu znakow podanego przez uzytkownika per spacja
+
+print('Uzytkowniku napisz zdanie skladajace sie z minimum dwoch slow')
+sent=input()
+print(sent.split(' '))
+
+#jeżeli splitter nie znajdzie znaku po którym ma splitowac-wrzuca do listy całą wartość jako jeden ciag
+print(sent.split(';'))
+
+
+
